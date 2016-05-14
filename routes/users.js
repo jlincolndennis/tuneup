69b45a7017d4bfb52e074e9313e4a6012fb13eff
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-05-13 19:46:44 -0600

    client side login working

--- routes/users.js
+++ routes/users.js
@@ -34,9 +34,9 @@ router.post('/signup', function (req, res, next) {
           .returning('*')
           .then(function (users) {
             const user = users[0];
-            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ user_id: user.user_id }, process.env.JWT_SECRET);
             res.json({
-              id: user.id,
+              user_id: user.user_id,
               username: user.username,
               email: user.email,
               token: token
@@ -53,6 +53,38 @@ router.post('/signup', function (req, res, next) {
 })
 
 router.post('/login', function (req, res, next) {
+  const errors = []
+
+  if (!req.body.email || !req.body.email.trim()) errors.push("Email can't be blank");
+  if (!req.body.password || !req.body.password.trim()) errors.push("Password can't be blank");
+
+  if (errors.length) {
+    res.status(422).json({
+      errors: errors
+    })
+  } else {
+    knex('users')
+    .whereRaw('lower(email) = ?', req.body.email.toLowerCase())
+    .first()
+    .then(function (user) {
+      console.log(user);
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        const token = jwt.sign({ user_id: user.user_id }, process.env.JWT_SECRET);
+        res.json({
+          user_id: user.id,
+          username: user.username,
+          email: user.email,
+          token: token
+        })
+      } else {
+        res.status(400).json({
+          errors: ['Wrong Password!']
+        })
+      }
+
+
+    })
+  }
 
 })
 
